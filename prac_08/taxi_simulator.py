990fc81bcc5cf8a99a4f9ae93a52891f1afcc784
alwingeorge <42425214+alwingeorge@users.noreply.github.com>
2018-10-18 13:53:39 +1000

    display taxis

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -4,6 +4,10 @@ from prac_08.silver_service_taxi import SilverServiceTaxi
 
 MENU = "q)uit, c)hoose taxi, d)rive"
 
+def display_taxis(taxis):
+    """Display numbered list of taxis."""
+    for i, taxi in enumerate(taxis):
+        print("{} - {}".format(i, str(taxi)))
 
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
@@ -13,7 +17,10 @@ def main():
     user_input = input(">>>").lower()
     while user_input != "q":
         if user_input == "c":
-            print("Taxis available")
+            print("Taxis available: ")
+            display_taxis(taxis)
+            taxi_choice = int(input("Choose taxi: "))
+            current_taxi = taxis[taxi_choice]
         elif user_input == "d":
             print("Drive")
         else:
